0a5515abbf7ae3d97d7143a3e5916927607bb93b
Cesc Delgado <cdelgado@omatech.com>
2019-10-27 20:24:04 +0100

    [CHANGE] name group

--- config/config.php
+++ config/config.php
@@ -5,8 +5,12 @@ if (env('APP_LOG_DRIVER_AWS', false)) {
     if (empty(env('APP_LOG_DRIVER_AWS_GROUP'))) {
         throw new Exception('No defined APP_LOG_DRIVER_AWS_GROUP');
     }
+    $group = env('APP_LOG_DRIVER_AWS_GROUP') . '/' . strtolower(env('SERVICE_NAME'));
+    if (!empty(env('SERVICE_NAME_CONTAINER'))) {
+        $group .= '/'.env('SERVICE_NAME_CONTAINER');
+    }
     $data['drivers']['cloudwatch'] = [
-        'group' => env('APP_LOG_DRIVER_AWS_GROUP'). '/' . strtolower(env('SERVICE_NAME')),
+        'group' => $group,
         'level' => env('APP_LOG_LEVEL', 'debug'),
         'retention' => env('APP_LOG_RETENTION', 140),
         'class' => Omatech\LaravelMonologExt\CloudWatch\CloudWatchLaravelLogging::class,
